accf12870dd94d357255d9eecfd9dfbf019547ca
Ahmed <abasimelahmed@gmail.com>
2025-09-07 12:23:22 +0200

    Added generator to batch_processing function()

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -33,9 +33,10 @@ def stream_users_in_batches(batch_size):
 ## Write a function batch_processing() that processes each batch to filter users over the age of 25
 def batch_processing(batch_size):
         for batch in stream_users_in_batches(batch_size):
-                filtered_users=[user for user in batch if user['age']>25]
-                for user in filtered_users:
-                        print(user)
+            for user in batch:
+                if user['age']>25:
+                    yield user
+                
                
 
 
