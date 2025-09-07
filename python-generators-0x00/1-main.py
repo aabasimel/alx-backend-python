3d0b9651e7d5e6d25f8b18a35fd254ab67d29d58
aabasimel <abasimelahmed@gmail.com>
2025-09-07 12:10:49 +0200

    Update 1-main.py

--- python-generators-0x00/1-main.py
+++ python-generators-0x00/1-main.py
@@ -3,6 +3,6 @@ processing = __import__('1-batch_processing')
 
 ##### print processed users in a batch of 50
 try:
-    processing.batch_processing(2)
+    processing.batch_processing(50)
 except BrokenPipeError:
     sys.stderr.close()
