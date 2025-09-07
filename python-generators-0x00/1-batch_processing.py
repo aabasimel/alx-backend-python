f588d49e1cdd4f65dbbeb70d761ff421c9d859bc
Ahmed <abasimelahmed@gmail.com>
2025-09-07 12:15:16 +0200

    Added 1-batch_processing.py

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -1,38 +1,38 @@
 import mysql.connector
 
-def batch_process_users(batch_size):
-        """Generator that streams rows from user_data in batches of batch_size"""
-        conn=None
-        cursor=None
-        try:
-                conn=mysql.connector.connect(
-                        host="localhost",
-                        user="root",
-                        password="Sh123&*abc",
-                        database="ALX_prodev"
-                )
-                cursor=conn.cursor(dictionary=True)
-                cursor.execute("SELECT *FROM user_data")
-                
-                batch=[]
-                for row in cursor:
-                        batch.append(row)
-                        if len(batch)==batch_size:
-                                yield batch
-                                batch=[]
-                if batch:
-                        yield batch
-        except Exception as e:
-                print(f"Error: {e}")
-        finally:
-                if cursor:
-                        cursor.close()
-                if conn and conn.is_connected():
-                        conn.close()
+def stream_users_in_batches(batch_size):
+    """
+    Generator that fetches rows from user_data in batches.
+    Each batch is a list of dictionaries (rows).
+    """
+    try:
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="deAlto#Crack357",
+            database="ALX_prodev"
+        )
+        cursor = connection.cursor(dictionary=True)
+        cursor.execute("SELECT * FROM user_data;")
 
-## Write a function batch_processing() that processes each batch to filter users over the age of25`
+        while True:
+            batch = cursor.fetchmany(batch_size)  # fetch a chunk of rows
+            if not batch:
+                break
+            yield batch  # yield the batch
+
+    except Exception as e:
+        print(f"Error: {e}")
+    finally:
+        if cursor:
+            cursor.close()
+        if connection and connection.is_connected():
+            connection.close()
+
+
+## Write a function batch_processing() that processes each batch to filter users over the age of 25
 def batch_processing(batch_size):
-        for batch in batch_process_users(batch_size):
+        for batch in stream_users_in_batches(batch_size):
                 filtered_users=[user for user in batch if user['age']>25]
                 for user in filtered_users:
                         print(user)
