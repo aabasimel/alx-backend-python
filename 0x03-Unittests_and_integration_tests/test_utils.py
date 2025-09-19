216326fd1054e0bdd0f36dd4fa202148a17157fe
Ahmed <abasimelahmed@gmail.com>
2025-09-19 11:49:59 +0200

    Initail commit

---
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+"""
+unit test for utils.py
+
+"""
+
+import unittest 
+
+from unittest.mock import patch
+from typing import Dict, Mapping, Sequence
+from parameterized import parameterized
+from utils import access_nested_map, get_json, memoize
+
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """Tests for access_nested_map."""
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2),
+    ])
+    def test_access_nested_map(self, nested_map:Mapping, path,expected):
+        """Test correct return values."""
+        self.assertEqual(access_nested_map(nested_map, path),expected)
+    
+    @parameterized.expand([
+         ({}, ("a",),),
+        ({"a": 1}, ("a", "b")),
+    ])
+
+    def test_access_nested_map_exception(self, nested_map:Mapping, path):
+        """ Test that KeyError is raised for invalid paths."""
+        with self.assertRaises(KeyError):
+            access_nested_map(nested_map, path)
+
+
+if __name__ == "__main__":
+    unittest.main()
+
+
