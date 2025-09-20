e1292b0b53ed7b32155ffc6187ca44c1f4ae00a7
Ahmed <abasimelahmed@gmail.com>
2025-09-20 19:30:01 +0200

    Initial commit

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -61,5 +61,34 @@ class TestGetJson(unittest.TestCase):
         mock_get.assert_called_once_with(test_url)
 
 
+class TestMemoize(unittest.TestCase):
+    """Test case for the memoize decorator."""
+
+    def test_memoize(self):
+        """Test that memoize caches the result of a method."""
+
+        class TestClass:
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        # Patch a_method so we can track calls
+        with patch.object(TestClass, "a_method", return_value=42) as mock_method:
+            obj = TestClass()
+
+            # Call a_property twice
+            result1 = obj.a_property
+            result2 = obj.a_property
+
+            # Both calls should return the same value
+            self.assertEqual(result1, 42)
+            self.assertEqual(result2, 42)
+
+            # But a_method should have only been called once
+            mock_method.assert_called_once()
+
 if __name__ == "__main__":
     unittest.main()
