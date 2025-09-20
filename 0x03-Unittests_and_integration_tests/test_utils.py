ee9994c8f3d707e5a931a20ff82e90b0ec3fa5ef
Ahmed <abasimelahmed@gmail.com>
2025-09-20 11:20:23 +0200

    Added test_get_json

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -4,29 +4,35 @@
 Unit tests for utils.py
 """
 import unittest
-from unittest.mock import patch
+from unittest.mock import patch, Mock
 from typing import Mapping
 from parameterized import parameterized
 from utils import access_nested_map, get_json, memoize
 
 
 class TestAccessNestedMap(unittest.TestCase):
-
     """Tests for access_nested_map."""
 
-    @parameterized.expand([
-        ({"a": 1}, ("a",), 1),
-        ({"a": {"b": 2}}, ("a",), {"b": 2}),
-        ({"a": {"b": 2}}, ("a", "b"), 2),
-    ])
+    @parameterized.expand(
+        [
+            ({"a": 1}, ("a",), 1),
+            ({"a": {"b": 2}}, ("a",), {"b": 2}),
+            ({"a": {"b": 2}}, ("a", "b"), 2),
+        ]
+    )
     def test_access_nested_map(self, nested_map: Mapping, path, expected):
         """Test correct return values."""
         self.assertEqual(access_nested_map(nested_map, path), expected)
 
-    @parameterized.expand([
-        ({}, ("a",),),
-        ({"a": 1}, ("a", "b")),
-    ])
+    @parameterized.expand(
+        [
+            (
+                {},
+                ("a",),
+            ),
+            ({"a": 1}, ("a", "b")),
+        ]
+    )
     def test_access_nested_map_exception(self, nested_map: Mapping, path):
         """Test that KeyError is raised for invalid paths."""
         with self.assertRaises(KeyError):
@@ -35,6 +41,24 @@ class TestAccessNestedMap(unittest.TestCase):
 
 class TestGetJson(unittest.TestCase):
     """Tests for get_json function."""
+
+    @parameterized.expand(
+        [
+            ("example", "http://example.com", {"payload": True}),
+            ("holberton", "http://holberton.io", {"payload": False}),
+        ]
+    )
+    @patch("utils.requests.get")
+    def test_get_json(self, name, test_url, test_payload, mock_get):
+        """Test that get_json returns the expected payload"""
+        # configure the mock to return a response with .json()=test_payload
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+
+        result = get_json(test_url)
+        self.assertEqual(result, test_payload)
+        mock_get.assert_called_once_with(test_url)
 
 
 if __name__ == "__main__":
