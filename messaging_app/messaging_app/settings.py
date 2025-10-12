1d22b34c3631e2d8ee0b27a62ae867cbb2a15817
Ahmed <abasimelahmed@gmail.com>
2025-10-12 16:28:50 +0200

    updated

--- messaging_app/messaging_app/settings.py
+++ messaging_app/messaging_app/settings.py
@@ -11,7 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import pymysql
+pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -79,8 +80,12 @@ WSGI_APPLICATION = "messaging_app.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'messages_db',       
+        'USER': 'aabasimel',      
+        'PASSWORD': 'password123',  
+        'HOST': 'localhost',          
+        'PORT': '3433', 
     }
 }
 
